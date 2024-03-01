5b89a9d46d52f0e7b0112b3afcba11261a13ec35
Lex Garey-Fernandez <lexgarey@gmail.com>
2024-03-01 09:24:29 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let generateButton = document.getElementById("generate-button")
+let passwordOneEl = document.getElementById("password-one")
+let passwordTwoEl = document.getElementById("password-two")
+
+generateButton.addEventListener("click", function() {
+    let passwordLength = 15
+
+   function getRandomCharacter() {
+        let index = Math.floor(Math.random() * characters.length)
+        return characters[index]
+   }
+
+   function generateRandomPassword() {
+        let password = ""
+        for (i = 0; i < passwordLength; i++) {
+            password += getRandomCharacter()
+        }
+        return password
+   }
+
+   let firstPassword = generateRandomPassword()
+   let secondPassword = generateRandomPassword()
+
+
+   passwordOneEl.textContent = firstPassword
+   passwordTwoEl.textContent = secondPassword
+
+
+})
+
